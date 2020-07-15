22093b5ad8d170ebeb53f9467b724f21a22ebf1f
AIman Baharum <aiman.baharum@outlook.com>
2020-07-15 21:05:25 +0800

    deleted view because frontend is going to handle in different module

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var entriesRouter = require('./routes/entries');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
