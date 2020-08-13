4a274a858726f32eb8a33da1c807b4fd4522edd1
AIman Baharum <aiman.baharum@outlook.com>
2020-08-14 01:29:06 +0800

    Moar error handling

--- controllers/entries.controller.js
+++ controllers/entries.controller.js
@@ -25,7 +25,8 @@ module.exports = {
         const { page, size } = req.query;
         const { limit, offset } = util.getPagination(page, size);
 
-        if (page < 0 || size < 0) return res.status(400).send({ message: 'Page/size must be positive number'});
+        if (page < 0 || size < 0 || !parseInt(page) || !parseInt(size))
+        return res.status(400).send({ message: 'Page/size must be positive number'});
 
         return repository.list({ limit, offset })
         .then(entries => {
